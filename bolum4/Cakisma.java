d920b9872fe30235e301335ec0546559a87ae545
altuga@gmail.com <upux1234>
2016-11-23 18:43:13 +0200

    Kod iyileştirmeleri yapıldı

--- bolum4/Cakisma.java
+++ bolum4/Cakisma.java
@@ -1,15 +1,16 @@
 package bolum4;
 
 import java.util.*;
+
 import com.kodcu.util.*;
 
-public class Cakisma  {
+public class Cakisma {
 
     public static void main(String args[]) {
-	System.out.println("Baslagic..");
+        System.out.println("Baslagic..");
 
-	ArrayList al = new ArrayList();
-	System.out.println("Bitis..");
+        //ArrayList al = new ArrayList();
+        System.out.println("Bitis..");
 
-    }  
+    }
 }
